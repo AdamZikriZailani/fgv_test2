338c233d0885e06b8ac3a7fd76a486dd22f91817
adamzikrizailani <adamzikrizailani@protonmail.com>
2024-09-11 11:03:52 +0800

    first commit

---
+++ catkin_ws/build/my_new_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/GitRepositories/fgv_test2/catkin_ws/src/my_new_package
+# Build directory: /home/user/GitRepositories/fgv_test2/catkin_ws/build/my_new_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
